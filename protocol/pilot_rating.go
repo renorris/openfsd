c0cc3d218ee36d7c5fc96c4f26d62c56fbe043f6
Reese Norris <reese.norris@gmail.com>
2024-10-22 12:24:17 -0700

    clean enums for parity

--- protocol/pilot_rating.go
+++ protocol/pilot_rating.go
@@ -13,23 +13,23 @@ const (
 )
 
 var pilotRatingToLongString = map[PilotRating]string{
-	0:  "Basic Member",
-	1:  "Private Pilot License",
-	3:  "Instrument Rating",
-	7:  "Commercial Multi-Engine License",
-	15: "Airline Transport Pilot License",
-	31: "Flight Instructor",
-	63: "Flight Examiner",
+	PilotRatingNEW:  "Basic Member",
+	PilotRatingPPL:  "Private Pilot License",
+	PilotRatingIR:   "Instrument Rating",
+	PilotRatingCMEL: "Commercial Multi-Engine License",
+	PilotRatingATPL: "Airline Transport Pilot License",
+	PilotRatingFI:   "Flight Instructor",
+	PilotRatingFE:   "Flight Examiner",
 }
 
 var pilotRatingToShortString = map[PilotRating]string{
-	0:  "NEW",
-	1:  "PPL",
-	3:  "IR",
-	7:  "CMEL",
-	15: "ATPL",
-	31: "FI",
-	63: "FE",
+	PilotRatingNEW:  "NEW",
+	PilotRatingPPL:  "PPL",
+	PilotRatingIR:   "IR",
+	PilotRatingCMEL: "CMEL",
+	PilotRatingATPL: "ATPL",
+	PilotRatingFI:   "FI",
+	PilotRatingFE:   "FE",
 }
 
 func ForEachPilotRating(f func(id PilotRating, shortString, longString string)) {
