2767fe9f30df3a7f33ae15d92ba2d49406648ab6
Reese Norris <reese.norris@gmail.com>
2025-05-23 20:21:16 -0700

    fix bat

--- run-windows.bat
+++ run-windows.bat
@@ -7,4 +7,3 @@ start /b cmd /c "set DATABASE_AUTO_MIGRATE=true&& set DATABASE_SOURCE_NAME=openf
 powershell -Command "while (-not (Test-NetConnection -ComputerName localhost -Port 13618 -InformationLevel Quiet)) { Start-Sleep -Seconds 1 }"
 
 cmd /c "cd web&& set FSD_HTTP_SERVICE_ADDRESS=http://localhost:13618&& set DATABASE_SOURCE_NAME=../openfsd.db?_pragma=busy_timeout(5000)^&_pragma=journal_mode(WAL)&& go run ."
-start /b cd web && go run .
